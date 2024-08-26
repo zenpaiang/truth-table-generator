61b4a3d363965fd5e1f503b5c3643d0f2918f890
zenpai <zenpaiang@gmail.com>
2024-08-26 21:03:30 +0800

    changed example to fit README

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import logic
 
 generator = logic.TruthTableGenerator()
 
-generated = generator.generate_truth_table("a & b")
+generated = generator.generate_truth_table("a & b | c # d")
 
 for result in generated:
     print(result, generated[result])
